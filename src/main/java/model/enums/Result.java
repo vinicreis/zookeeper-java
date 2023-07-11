033d9f880a0d6e9198d0964fb550c46263cff58d
Vinícius Reis <vnc.reis@outlook.com>
2023-07-10 23:28:22 -0300

    [WIP] Code documentation

--- src/main/java/model/enums/Result.java
+++ src/main/java/model/enums/Result.java
@@ -1,9 +1,27 @@
 package model.enums;
 
+/**
+ * Enum class that maps all possible results to be returned to clients.
+ */
 public enum Result {
+    /**
+     * Indicates that the request finished with no errors.
+     */
     OK,
+    /**
+     * Indicates that some business or validation error occurred on the target.
+     */
     ERROR,
+    /**
+     * Indicates that the client should make the request to other server to fetch an updated value from a key.
+     */
     TRY_OTHER,
+    /**
+     * Indicates that the request key was not found on the server.
+     */
     NOT_FOUND,
+    /**
+     * Indicates that an unexpected exception/error happened during the execution.
+     */
     EXCEPTION
 }
--- src/main/java/model/exception/OutdatedEntryException.java
+++ src/main/java/model/exception/OutdatedEntryException.java
@@ -1,5 +1,8 @@
 package model.exception;
 
+/**
+ * Exception to be thrown in case a key with an outdated timestamp tries to be fetch.
+ */
 public class OutdatedEntryException extends Exception {
     private final String key;
     private final Long currentTimestamp;
@@ -9,10 +12,18 @@ public class OutdatedEntryException extends Exception {
         this.currentTimestamp = currentTimestamp;
     }
 
+    /**
+     * Get the requested key with error.
+     * @return the key fetched with an outdated timestamp.
+     */
     public String getKey() {
         return key;
     }
 
+    /**
+     * Gets the timestamp that the user has associated with this key.
+     * @return the integer timestamp value
+     */
     public Long getCurrentTimestamp() {
         return currentTimestamp;
     }
